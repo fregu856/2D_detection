e2c1215aeabdbbeac845415b020f9353434bf80a
fregu856 <fregu856@student.liu.se>
2017-09-23 17:36:57 +0200

    Commenting done.

--- run_on_KITTI_sequence.py
+++ run_on_KITTI_sequence.py
@@ -1,12 +1,7 @@
 import numpy as np
 import cPickle
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.core.protobuf import saver_pb2
 import cv2
-import random
 import os
 
 from model import SqueezeDet_model
@@ -27,9 +22,10 @@ img_height = model.img_height
 img_width = model.img_width
 no_of_classes = model.no_of_classes
 
+# load the mean color channels of the train imgs:
 train_mean_channels = cPickle.load(open("data/mean_channels.pkl"))
 
-# load the test data from disk
+# load the sequence data from disk
 frame_paths = cPickle.load(open(data_dir + "KITTI_seq_test_12_frame_paths.pkl"))
 
 # compute the number of batches needed to iterate through the data:
@@ -38,7 +34,7 @@ no_of_batches = int(no_of_frames/batch_size)
 
 results_dir = model.project_dir + "results_on_KITTI_seq/"
 
-# create a saver for saving all model variables/parameters:
+# create a saver for restoring variables/parameters:
 saver = tf.train.Saver(write_version=tf.train.SaverDef.V2)
 
 with tf.Session() as sess:
@@ -46,6 +42,7 @@ with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
 
+    # restore the best trained model:
     #saver.restore(sess, "/home/fregu856/2D_detection/training_logs/best_model/model_1_epoch_58.ckpt")
     saver.restore(sess, "/root/2D_detection/training_logs/best_model/model_1_epoch_58.ckpt")
 
@@ -72,8 +69,8 @@ with tf.Session() as sess:
         print "step: %d/%d" % (step + 1, no_of_batches)
 
         for i in range(batch_size):
-            final_bboxes, final_probs, final_classes = model.filter_prediction(pred_bboxes[i],
-                        detection_probs[i], detection_classes[i])
+            final_bboxes, final_probs, final_classes = model.filter_prediction(
+                        pred_bboxes[i], detection_probs[i], detection_classes[i])
 
             keep_idx = [idx for idx in range(len(final_probs)) if final_probs[idx] > model.plot_prob_thresh]
             final_bboxes = [final_bboxes[idx] for idx in keep_idx]
@@ -81,13 +78,16 @@ with tf.Session() as sess:
             final_classes = [final_classes[idx] for idx in keep_idx]
 
             # draw the bboxes outputed by the model:
-            pred_img = draw_bboxes(batch_imgs[i].copy()+train_mean_channels, final_bboxes, final_classes, final_probs)
+            pred_img = draw_bboxes(batch_imgs[i].copy()+train_mean_channels,
+                        final_bboxes, final_classes, final_probs)
             img_name = img_paths[i].split("/")[-1]
             pred_path = results_dir + img_name.split(".png")[0] + "_pred.png"
             cv2.imwrite(pred_path, pred_img)
 
+# create a video of all the resulting images:
 fourcc = cv2.cv.CV_FOURCC("M", "J", "P", "G")
-out = cv2.VideoWriter(results_dir + "KITTI_seq_test_12_pred.avi", fourcc, 10.0, (img_width, img_height))
+out = cv2.VideoWriter(results_dir + "KITTI_seq_test_12_pred.avi", fourcc, 10.0,
+            (img_width, img_height))
 
 frame_names = sorted(os.listdir(results_dir))
 for step, frame_name in enumerate(frame_names):
